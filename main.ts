369af02ca1a2e1df4a33326276b4eeafd16371c1
itaparcade <itaparcade@users.noreply.github.com>
2021-01-17 14:25:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    count = 0
+    basic.showString("HOOPS!")
+    basic.showIcon(IconNames.Square)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    music.playMelody("C D E F G A B C5 ", 1000)
+    count += 1
+    basic.showNumber(count)
+    if (count == 7) {
+        basic.showIcon(IconNames.Surprised)
+        for (let index = 0; index < 4; index++) {
+            music.playMelody("G B A G C5 B A B ", 1000)
+        }
+        basic.showIcon(IconNames.Square)
+        basic.pause(2000)
+        count = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(262, music.beat(BeatFraction.Double))
+    if (count > 0) {
+        count += -1
+        if (count == 0) {
+            basic.showIcon(IconNames.Square)
+        } else {
+            basic.showNumber(count)
+        }
+    }
+})
+let count = 0
+count = 0
+basic.showString("HOOPS!")
+music.playMelody("C5 G B A F A C5 B ", 1000)
+basic.showIcon(IconNames.Square)
+basic.forever(function () {
+	
+})
