10c8e9ae5f0ed3f9ec789321aed316fa2894044d
itaparcade <itaparcade@users.noreply.github.com>
2021-05-29 20:42:58 -0400

    Updated detection approach

--- main.ts
+++ main.ts
@@ -15,25 +15,25 @@ input.onButtonPressed(Button.B, function () {
         }
     }
 })
-input.onPinPressed(TouchPin.P1, function () {
-    music.playMelody("C D E F G A B C5 ", 1000)
-    count += 1
-    basic.showNumber(count)
-    if (count == 7) {
-        basic.showIcon(IconNames.Surprised)
-        for (let index = 0; index < 4; index++) {
-            music.playMelody("G B A G C5 B A B ", 1000)
-        }
-        basic.showIcon(IconNames.Square)
-        basic.pause(2000)
-        count = 0
-    }
-})
 let count = 0
+pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 count = 0
 basic.showString("HOOPS ARCADE!")
 music.playMelody("C5 G B A F A C5 B ", 1000)
 basic.showIcon(IconNames.Square)
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        music.playMelody("C D E F G A B C5 ", 1000)
+        count += 1
+        basic.showNumber(count)
+        if (count == 7) {
+            basic.showIcon(IconNames.Surprised)
+            for (let index = 0; index < 4; index++) {
+                music.playMelody("G B A G C5 B A B ", 1000)
+            }
+            basic.showIcon(IconNames.Square)
+            basic.pause(2000)
+            count = 0
+        }
+    }
 })
